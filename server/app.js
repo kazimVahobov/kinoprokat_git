d12e80531584978ea894951bdae4e456d33685fe
kazimVahobov <penzakokand@gmail.com>
2019-04-04 14:42:06 +0500

    on line 57 change => to function

--- server/app.js
+++ server/app.js
@@ -54,7 +54,7 @@ app.use('/api/distributor-report', distributorReportsRoutes)
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/dist/client'))
   
-    app.get('*', (req, res) => {
+    app.get('*', function(req, res) {
       res.sendFile(
         path.resolve(
           __dirname, 'client', 'dist', 'client', 'index.html'
