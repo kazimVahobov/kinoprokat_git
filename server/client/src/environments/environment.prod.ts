3cb5dcd4f5e0adcb2b3dad76c15c6b1a06c1f5a4
kazimVahobov <penzakokand@gmail.com>
2019-04-04 20:44:30 +0500

    change port in environment.prod

--- server/client/src/environments/environment.prod.ts
+++ server/client/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   version: "1.0.0",
   // apiUrl: 'http://192.168.43.179:5000',
-  apiUrl: 'http://localhost:5000'
+  apiUrl: 'http://localhost:8080'
 };
