0fd19e37e0536af823ad5ff8d500702621367552
kazimVahobov <penzakokand@gmail.com>
2019-05-23 13:22:26 +0500

    Merge branch 'master' of https://github.com/kazimVahobov/kinoprokat_git
    # Conflicts:
    #	server/client/src/app/app.component.ts

--- server/client/src/app/app.component.ts
+++ server/client/src/app/app.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
 }
