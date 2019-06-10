b28f91f8b86b2fece6e0afff9bdaa3eff153e9c2
kazimVahobov <penzakokand@gmail.com>
2019-06-10 11:28:23 +0500

    restyling of login page

--- server/app.js
+++ server/app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+﻿const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const bodyParser = require('body-parser');
